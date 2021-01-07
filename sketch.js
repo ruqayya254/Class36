98f9ec6321068e5c873ed54f5268f7c03f7de0f8
ruqayya254 <72867417+ruqayya254@users.noreply.github.com>
2021-01-07 19:55:49 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+var hypnoticBall, database;
+var position;
+var gameState=0;
+var playerCount
+var database,form,player,game
+
+
+function setup(){
+  database = firebase.database();
+  createCanvas(400,400);
+
+  game=new Game()
+  game.getState()
+  game.start()
+  
+}
+
+function draw(){
+  background("white");
+  
+    
+    drawSprites();
+  
+}
+
+
+
+
+
